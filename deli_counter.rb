e4da3f7b47fa52f283daf15c1cabb56e945bc6d7
Robert Stine <rcorystine@gmail.com>
2020-12-17 23:12:28 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,5 +8,5 @@ def line(customer)
     customer.each_with_index |name, index|
       customer_line.push("#{index}. #{name}")
   end
-  puts "The line is currently: "
+  puts "The line is currently: #{customer_line
 end
