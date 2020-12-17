51874c43090e71bb1f81ec3681c00135fea13f5d
Robert Stine <rcorystine@gmail.com>
2020-12-17 23:09:14 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,8 @@ def line(customer)
   if customer.length == 0 
     puts "The line is currently empty."
   elsif customer.length > 0 
-    customer.each_with_index
+    customer.each_with_index |name, index|
+      
     
   end
 end
