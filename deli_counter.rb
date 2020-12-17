8c4724e9f1227a16e6bbd24b672b2b7313f869e6
Robert Stine <rcorystine@gmail.com>
2020-12-17 23:35:21 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,4 +17,8 @@ def take_a_number(katz_deli, name)
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
-def now_serving
+def now_serving(deli)
+  if deli.length == 0 
+    puts "There is nobody waiting to be served!"
+  else
+    
