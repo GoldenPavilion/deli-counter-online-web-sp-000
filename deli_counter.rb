51ed8f6d9d1f8d154a337d829fd6bc6579abae55
Robert Stine <rcorystine@gmail.com>
2020-12-17 23:15:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,10 +4,10 @@ def line(customer)
   customer_line = []
   if customer.length == 0 
     puts "The line is currently empty."
-    elsif customer.length > 0 
+  elsif customer.length > 0 
       customer.each_with_index |name, index|
-      customer_line.push("#{index}. #{name}")
-    end
+        customer_line.push("#{index}. #{name}")
+      end
   puts "The line is currently: #{customer_line.join(" ")}"
   end
 end
