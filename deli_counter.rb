acfd103922738666d51de65e11d9a14351f88739
Robert Stine <rcorystine@gmail.com>
2020-12-17 23:20:30 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,13 +1,14 @@
 katz_deli = []
 
-def line(customer)
-  customer_line = []
-  if customer.length == 0 
-    puts "The line is currently empty."
-  else
-      customer.each_with_index |name, index|
-        customer_line.push("#{index}. #{name}")
-      end
-  puts "The line is currently: #{customer_line.join(" ")}"
-  end
-end
+#def line(customer)
+  #customer_line = []
+  #if customer.length == 0 
+    #puts "The line is currently empty."
+  #else
+      #customer.each_with_index |name, index|
+        #customer_line.push("#{index}. #{name}")
+      #end
+  #puts "The line is currently: #{customer_line.join(" ")}"
+  #end
+#end
+
